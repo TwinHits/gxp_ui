6db78c07d8854a2af59d58f92870fd208fd6644c
John Goben <TwinHits@gmail.com>
2022-08-15 19:14:45 -0400

    Added IconButton component.

---
+++ src/views/common/IconButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-btn icon @click="$emit('click')"> 
+        <v-icon>
+            {{ icon }}
+        </v-icon>
+    </v-btn>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    props: {
+        icon: {
+            type: String,
+            required: true,
+        }
+    }    
+})
+</script>
+
+<style scoped>
+
+</style>
