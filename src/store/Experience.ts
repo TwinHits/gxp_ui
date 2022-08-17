6e0c293cbd1247ae9ea1056fd2ee76b6a542460c
John Goben <TwinHits@gmail.com>
2022-08-17 09:38:56 -0400

    Fixed getting next experience level issue when there is no next level.

--- src/store/Experience.ts
+++ src/store/Experience.ts
@@ -48,13 +48,16 @@ export default {
                 return state.experienceLevels[state.experienceLevels.length - 2];
             }
 
+            // Return highest experience level if there is no next
+            if (experience >= state.experienceLevels[0].experience_required) {
+                return state.experienceLevels[0]
+            }
+
             for (const [index, level] of state.experienceLevels.entries()) {
                 if (experience >= level.experience_required) {
                     return state.experienceLevels[index - 1];
                 }
             }
-            // Return highest experience level if there is no next
-            return state.experienceLevels[0];
         },
         experienceEvent: (state: any) => (id: string) => {
             return state.experienceEvents[id];
